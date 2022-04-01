9f92634eed82a9f81efd4bfb2fdf6094bc9ddfc3
Hugo <24863815+bgpvp@users.noreply.github.com>
2022-04-02 01:01:50 +0200

    🐛 Will I fix this build?

--- build.js
+++ build.js
@@ -1,6 +1,26 @@
-const execSync = require("child_process").execSync;
+const browserify = require("browserify");
+const envify = require("envify");
+const fs = require('fs');
 require("dotenv").config();
 
+browserify()
+    .add("./public/fullscreen/script-before-build.js")
+    .transform(envify({
+        API_KEY: process.env.API_KEY,
+        SECRET: process.env.SECRET
+    }))
+    .bundle()
+    .pipe(fs.createWriteStream("./public/fullscreen/script.js"))
+
+browserify()
+    .add("./public/login/script-before-build.js")
+    .transform(envify({
+        API_KEY: process.env.API_KEY
+    }))
+    .bundle()
+    .pipe(fs.createWriteStream("./public/login/script.js"))
+
+/*
 try {
     execSync(`npm install -g browserify envify`);
     console.log("[Fullscreen Spotify] Building the fullscreen script...");
@@ -10,4 +30,4 @@ try {
     console.log("[Fullscreen Spotify] Scripts are built!")
 } catch (err) {
     console.log(err);
-}
+}*/
