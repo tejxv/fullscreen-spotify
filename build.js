1e569909deaf1b11bbc3cad6d82614b34da8ee6e
Hugo <24863815+bgpvp@users.noreply.github.com>
2022-04-02 00:44:45 +0200

    🐛 Fixed the build script that should make it work

--- build.js
+++ build.js
@@ -1,6 +1,12 @@
-const exec = require("child_process").exec;
+const execSync = require("child_process").execSync;
 require("dotenv").config();
-console.log("Building the fullscreen script...")
-exec(`npx browserify ./public/fullscreen/script-before-build.js -t [ envify --API_KEY ${process.env.API_KEY} --SECRET ${process.env.SECRET} ] > ./public/fullscreen/script.js`);
-console.log("Building the login script...")
-exec(`npx browserify ./public/login/script-before-build.js -t [ envify --API_KEY ${process.env.API_KEY} ] > ./public/login/script.js`);
+
+try {
+    console.log("[Fullscreen Spotify] Building the fullscreen script...");
+    execSync(`npx browserify ./public/fullscreen/script-before-build.js -t [ envify --API_KEY ${process.env.API_KEY} --SECRET ${process.env.SECRET} ] > ./public/fullscreen/script.js`);
+    console.log("[Fullscreen Spotify] Building the login script...");
+    execSync(`npx browserify ./public/login/script-before-build.js -t [ envify --API_KEY ${process.env.API_KEY} ] > ./public/login/script.js`);
+    console.log("[Fullscreen Spotify] Scripts are built!")
+} catch (err) {
+    console.log(err);
+}
