fe42ee5a88a0295cbb1f04f57592c98d2530b6b6
Hugo <24863815+bgpvp@users.noreply.github.com>
2022-04-02 15:00:03 +0200

    added npx

--- build.js
+++ build.js
@@ -1,11 +1,10 @@
 const execSync = require("child_process").execSync;
 
 try {
-    execSync(`npm install -g browserify envify`);
     console.log("[Fullscreen Spotify] Building the fullscreen script...");
-    execSync(`browserify ./public/fullscreen/script-before-build.js -t [ envify --API_KEY ${process.env.API_KEY} --SECRET ${process.env.SECRET} ] > ./public/fullscreen/script.js`);
+    execSync(`npx browserify ./public/fullscreen/script-before-build.js -t [ envify --API_KEY ${process.env.API_KEY} --SECRET ${process.env.SECRET} ] > ./public/fullscreen/script.js`);
     console.log("[Fullscreen Spotify] Building the login script...");
-    execSync(`browserify ./public/login/script-before-build.js -t [ envify --API_KEY ${process.env.API_KEY} ] > ./public/login/script.js`);
+    execSync(`npx browserify ./public/login/script-before-build.js -t [ envify --API_KEY ${process.env.API_KEY} ] > ./public/login/script.js`);
     console.log("[Fullscreen Spotify] Scripts are built!")
 } catch (err) {
     console.log(err);
