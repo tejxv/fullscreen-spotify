93c61bc5ad6b7aa046c556b1de83b9710baa84d5
Hugo <24863815+bgpvp@users.noreply.github.com>
2022-04-02 19:06:17 +0200

    ⏪ Revert to 1.0.1

--- build.js
+++ build.js
@@ -1,15 +1,6 @@
 const exec = require("child_process").exec;
 require("dotenv").config();
-
-console.log(process.env.API_KEY);
-console.log(process.env.SECRET);
-
-console.log("[Fullscreen Spotify] Building the fullscreen script...");
-exec(
-  `npx browserify ./public/fullscreen/script-before-build.js -t [ envify --API_KEY ${process.env.API_KEY} --SECRET ${process.env.SECRET} ] > ./public/fullscreen/script.js`
-);
-console.log("[Fullscreen Spotify] Building the login script...");
-exec(
-  `npx browserify ./public/login/script-before-build.js -t [ envify --API_KEY ${process.env.API_KEY} ] > ./public/login/script.js`
-);
-console.log("[Fullscreen Spotify] Scripts are built!");
+console.log("Building the fullscreen script...")
+exec(`npx browserify ./public/fullscreen/script-before-build.js -t [ envify --API_KEY ${process.env.API_KEY} --SECRET ${process.env.SECRET} ] > ./public/fullscreen/script.js`);
+console.log("Building the login script...")
+exec(`npx browserify ./public/login/script-before-build.js -t [ envify --API_KEY ${process.env.API_KEY} ] > ./public/login/script.js`);
