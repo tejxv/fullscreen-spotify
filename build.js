e27a57df2a5519aaa63fe118f5cbe423940a96a0
Hugo <24863815+bgpvp@users.noreply.github.com>
2022-04-02 15:15:10 +0200

    debug 2

--- build.js
+++ build.js
@@ -1,16 +1,15 @@
 const exec = require("child_process").exec;
-require("dotenv").config()
+require("dotenv").config();
 
-console.log(process.env)
-console.log(process.env.API_KEY)
-console.log(process.env.SECRET)
+console.log(process.env.API_KEY);
+console.log(process.env.SECRET);
 
-try {
-    console.log("[Fullscreen Spotify] Building the fullscreen script...");
-    exec(`npx browserify ./public/fullscreen/script-before-build.js -t [ envify --API_KEY ${process.env.API_KEY} --SECRET ${process.env.SECRET} ] > ./public/fullscreen/script.js`);
-    console.log("[Fullscreen Spotify] Building the login script...");
-    exec(`npx browserify ./public/login/script-before-build.js -t [ envify --API_KEY ${process.env.API_KEY} ] > ./public/login/script.js`);
-    console.log("[Fullscreen Spotify] Scripts are built!")
-} catch (err) {
-    console.log(err);
-}
+console.log("[Fullscreen Spotify] Building the fullscreen script...");
+exec(
+  `npx browserify ./public/fullscreen/script-before-build.js -t [ envify --API_KEY ${process.env.API_KEY} --SECRET ${process.env.SECRET} ] > ./public/fullscreen/script.js`
+);
+console.log("[Fullscreen Spotify] Building the login script...");
+exec(
+  `npx browserify ./public/login/script-before-build.js -t [ envify --API_KEY ${process.env.API_KEY} ] > ./public/login/script.js`
+);
+console.log("[Fullscreen Spotify] Scripts are built!");
